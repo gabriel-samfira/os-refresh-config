ae588f91e6463d62dedc185eff0137e8c31f14f2
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2015-01-07 00:41:04 -0800

    path consistency

--- os_refresh_config/paths_windows.py
+++ os_refresh_config/paths_windows.py
@@ -3,6 +3,6 @@ import os
 TMPDIR = os.environ.get("TEMP", "C:\\")
 DEFAULT_LOCK_FILE = os.path.join(TMPDIR, "os-refresh-config.lock")
 
-DEFAULT_BASE_DIR = 'C:\\tripleo\\os-refresh-config'
+DEFAULT_BASE_DIR = 'C:\\tripleo\\libexec\\os-refresh-config'
 # For compatibillity
 OLD_BASE_DIR = DEFAULT_BASE_DIR
